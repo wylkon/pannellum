91b39cd8d59891dc1dac17678ebf7389c4e95d33
Wylkon Cardoso <wylkon.cardoso@amaro.com>
2020-08-15 21:29:53 -0300

    make available import pannellum on react

---
+++ build/index.js
@@ -0,0 +1,1 @@
+"use strict";require("./libpannellum"),require("./pannellum"),Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=pannellum;
---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+require("./libpannellum");
+require("./pannellum");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+});
+
+exports.default = pannellum;
